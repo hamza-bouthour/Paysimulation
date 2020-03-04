ccf685f76bd593d53c8fe0235ea4879505c971e5
Hamza Bouthour <h.bouthour@gmail.com>
2020-03-04 11:02:39 -0800

    css, html, js edited

---
+++ program.js
@@ -0,0 +1,27 @@
+
+
+
+// //function grossPay() {
+//     var hp = document.getElementById("hourlyPay").Value;
+//     var th = document.getElementById("totalHours").Value;
+//     if (th <= 40) { 
+//         return hp * th
+        
+//     }
+//     else 
+//     return (th - 40) * (hp * 1.5) + (40 * hp)
+
+// }//
+// document.getElementById("result1").innerHTML = grossPay();//
+
+function grossPay() {
+    var hp = document.getElementById("hourlyPay").Value;
+    var th = document.getElementById("totalHours").Value;
+    if (th != null) { 
+        return true
+        
+    }
+    
+
+}
+document.getElementById("result1").innerHTML = grossPay();
