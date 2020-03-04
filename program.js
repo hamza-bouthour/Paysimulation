7d815d146bd81a1a0f992e447d3309547cd7fb4b
Hamza Bouthour <h.bouthour@gmail.com>
2020-03-04 15:22:15 -0800

    changes on HTML and JS

--- program.js
+++ program.js
@@ -1,17 +1,32 @@
+// var form = document.getElementById("myForm");
+// function handleForm(event) { event.preventDefault(); } 
+// form.addEventListener('submit', handleForm());
 
 
-
+// function asba() {
+//     document.getElementById("result1").innerHTML = 'ye miboun';
+// }
+// document.getElementById("calculate1").addEventListener("click", asba());
 function grossPay() {
+    console.log('ekhdem nayek');
     var hp = document.getElementById("hourlyPay").value;
     var th = document.getElementById("totalHours").value;
     if (hp <= 40) { 
-        document.getElementById("result1").innerHTML = hp * th
+        document.getElementById("result1").innerHTML = hp * th;
+        console.log(hp * th);
         
     }
     else 
-    return (hp - 40) * (th * 1.5) + (40 * hp)
+    console.log((hp - 40) * (th * 1.5) + (40 * hp));
+    return (hp - 40) * (th * 1.5) + (40 * hp);
+    
 
 }
+//console.log('asba lik');
+//document.getElementById("calculate1").addEventListener("click", grossPay());
+
+//console.log(grossPay());
+//console.log('hello jappa')
 //document.getElementById("result1").innerHTML = grossPay();
 
 // function grossPay() {
@@ -23,3 +38,9 @@ function grossPay() {
 //     }
 // }
 // document.getElementById("result1").innerHTML = grossPay();
+// window.onload= function (){
+//     function asba() {
+//              document.getElementById("result1").innerHTML = 'ye miboun';
+//          }
+//         }        
+//         ` `
