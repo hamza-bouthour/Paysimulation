effcc95af66780a0c06b6d1283a1e9c653ae0f11
Hamza Bouthour <h.bouthour@gmail.com>
2020-03-04 12:47:11 -0800

    js, html edited

--- program.js
+++ program.js
@@ -1,27 +1,25 @@
 
 
 
-// //function grossPay() {
-//     var hp = document.getElementById("hourlyPay").Value;
-//     var th = document.getElementById("totalHours").Value;
-//     if (th <= 40) { 
-//         return hp * th
-        
-//     }
-//     else 
-//     return (th - 40) * (hp * 1.5) + (40 * hp)
-
-// }//
-// document.getElementById("result1").innerHTML = grossPay();//
-
 function grossPay() {
-    var hp = document.getElementById("hourlyPay").Value;
-    var th = document.getElementById("totalHours").Value;
-    if (th != null) { 
-        return true
+    var hp = document.getElementById("hourlyPay").value;
+    var th = document.getElementById("totalHours").value;
+    if (hp <= 40) { 
+        document.getElementById("result1").innerHTML = hp * th
         
     }
-    
+    else 
+    return (hp - 40) * (th * 1.5) + (40 * hp)
 
 }
-document.getElementById("result1").innerHTML = grossPay();
+//document.getElementById("result1").innerHTML = grossPay();
+
+// function grossPay() {
+//     var hp = document.getElementById("hourlyPay").Value;
+//     var th = document.getElementById("totalHours").Value;
+//     if (th != null) { 
+//         return true
+        
+//     }
+// }
+// document.getElementById("result1").innerHTML = grossPay();
