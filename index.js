89989ab0d60f3799912a5581e7c7992ee5628669
hamza bouthour <61101578+hamza-bouthour@users.noreply.github.com>
2020-03-24 21:13:17 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
+
 function grossPay(hourlyPay, totalHours) {
-    var hp = document.getElementById('hourlyPay').value;
-    var th = document.getElementById('totalHours').value;
-    var bn = document.getElementById('bonus').value;
-    if (th <= 40 && bn > 1) {
-        var res = parseFloat(bn) + parseFloat(th) * parseFloat(hp);
-        var tot = parseFloat(res) * 90 / 100;
-        document.getElementById("result1").innerHTML = ' $ ' + tot;
-    }
-    else if (th <= 40 && bn == 0) {
-        var res = parseFloat(th) * parseFloat(hp);
-        var tot = parseFloat(res) * 90 / 100;
-        document.getElementById("result1").innerHTML = ' $ ' + tot;
-    }
-    else if (th > 40 && bn > 0) {
-        var res = (parseFloat(th) - 40) * (parseFloat(hp) * 1.5) + (40 * parseFloat(hp)) + parseFloat(bn);
-        var tot = parseFloat(res) * 90 / 100;
-        document.getElementById("result1").innerHTML = ' $ ' + tot;
-
-    }
-    else
-        var res = (parseFloat(th) - 40) * (parseFloat(hp) * 1.5) + (40 * parseFloat(hp));
-        var tot = parseFloat(res) * 90 / 100;
-    document.getElementById("result1").innerHTML = ' $ ' + tot;
-
+  var hp = document.getElementById('hp1').value;
+  var th = document.getElementById('th1').value;
+  var bn = document.getElementById('b1').value;
+  var tot = 0
+  let res = 0
+  if (th <= 40 && bn > 1) {
+    res = parseFloat(bn) + parseFloat(th) * parseFloat(hp);
+    tot = parseFloat(res) * 90 / 100;
+    document.getElementById("asba").innerHTML = 'Your estimated pay is' + ' $ ' + tot;
+    console.log(tot);
+  }
+  else if (th <= 40 && bn == 0) {
+    res = parseFloat(th) * parseFloat(hp);
+    tot = parseFloat(res) * 90 / 100;
+    document.getElementById("asba").innerHTML = 'Your estimated pay is' + ' $ ' + tot;
+    console.log(tot);
+  }
+  else if (th > 40 && bn > 0) {
+    res = (parseFloat(th) - 40) * (parseFloat(hp) * 1.5) + (40 * parseFloat(hp)) + parseFloat(bn);
+    tot = parseFloat(res) * 90 / 100;
+    document.getElementById("asba").innerHTML = 'Your estimated pay is' + ' $ ' + tot;
+    console.log(tot);
+  }
+  else
+    res = (parseFloat(th) - 40) * (parseFloat(hp) * 1.5) + (40 * parseFloat(hp));
+    tot = parseFloat(res) * 90 / 100;
+    document.getElementById("asba").innerHTML = 'Your estimated pay is' + ' $ ' + tot;
+     console.log(tot);
 }
-grossPay();
+
+
+// $(document).ready(function(){
+//    $(function calculate() {
+//        $(input).click(function(){
+//            $(estimator1).append(<p>achbchsbchlsabcdjlhscb</p>);
+//        });
+//    });
+
+// });
+
