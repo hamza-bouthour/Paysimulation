67432aac8ce3b95ebcf77d24c33579b4e67c5801
hamza bouthour <61101578+hamza-bouthour@users.noreply.github.com>
2020-03-26 20:45:27 -0700

    Update index.js

--- index.js
+++ index.js
@@ -8,35 +8,47 @@ function grossPay(hourlyPay, totalHours) {
   if (th <= 40 && bn > 1) {
     res = parseFloat(bn) + parseFloat(th) * parseFloat(hp);
     tot = parseFloat(res) * 90 / 100;
-    document.getElementById("asba").innerHTML = 'Your estimated pay is' + ' $ ' + tot;
+    document.getElementById("amount").innerHTML = 'Your estimated pay is:' + ' $' + tot;
     console.log(tot);
   }
   else if (th <= 40 && bn == 0) {
     res = parseFloat(th) * parseFloat(hp);
     tot = parseFloat(res) * 90 / 100;
-    document.getElementById("asba").innerHTML = 'Your estimated pay is' + ' $ ' + tot;
+    document.getElementById("amount").innerHTML = 'Your estimated pay is:' + ' $' + tot;
     console.log(tot);
   }
   else if (th > 40 && bn > 0) {
     res = (parseFloat(th) - 40) * (parseFloat(hp) * 1.5) + (40 * parseFloat(hp)) + parseFloat(bn);
     tot = parseFloat(res) * 90 / 100;
-    document.getElementById("asba").innerHTML = 'Your estimated pay is' + ' $ ' + tot;
+    document.getElementById("amount").innerHTML = 'Your estimated pay is:' + ' $' + tot;
     console.log(tot);
   }
-  else
+  else 
     res = (parseFloat(th) - 40) * (parseFloat(hp) * 1.5) + (40 * parseFloat(hp));
     tot = parseFloat(res) * 90 / 100;
-    document.getElementById("asba").innerHTML = 'Your estimated pay is' + ' $ ' + tot;
+    document.getElementById("amount").innerHTML = 'Your estimated pay is:' + ' $' + tot;
      console.log(tot);
+  
 }
 
 
-// $(document).ready(function(){
-//    $(function calculate() {
-//        $(input).click(function(){
-//            $(estimator1).append(<p>achbchsbchlsabcdjlhscb</p>);
-//        });
-//    });
+$(document).ready(function(){
+ 
+  $('input[type="button"]').on('click', function(e){
 
-// });
+    e.preventDefault();
+    // e.preventPropagation();
+    // $('#asba').text('Your estimated pay is' + ' $ ' + tot);
+    $('div#result-window').removeClass('hidden').addClass('show-result-employee');
+    // $('div.input-box').addClass('hidden');
+    // $('employee-estimator').addClass('hidden');
+  });
+
+  $('button.restart').on('click', function() {
+    $('div#result-window').removeClass('show-result-employee').addClass('hidden');
+    
+  });
+  
+
+});
 
