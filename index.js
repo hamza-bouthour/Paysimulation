f1f13ae7e387aa02df150e22a112700bc57fa00a
hamza bouthour <61101578+hamza-bouthour@users.noreply.github.com>
2020-03-27 23:00:14 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,98 @@
 
-function grossPay(hourlyPay, totalHours) {
-  var hp = document.getElementById('hp1').value;
-  var th = document.getElementById('th1').value;
-  var bn = document.getElementById('b1').value;
-  var tot = 0
-  let res = 0
-  if (th <= 40 && bn > 1) {
-    res = parseFloat(bn) + parseFloat(th) * parseFloat(hp);
-    tot = parseFloat(res) * 90 / 100;
-    document.getElementById("amount").innerHTML = 'Your estimated pay is:' + ' $' + tot;
-    console.log(tot);
+function grossPayEmployee(hourlyPay, totalHours) {
+  let hourlyPayEmployee = document.getElementById('hourly-pay-employee').value;
+  let totalHoursEmployee = document.getElementById('total-hours-employee').value;
+  let bonusEmployee = document.getElementById('bonus-employee').value;
+  let totalAmount = 0
+  if (hourlyPayEmployee == null || hourlyPayEmployee == "" || totalHoursEmployee == null || totalHoursEmployee == "") {
+    return false;
   }
-  else if (th <= 40 && bn == 0) {
-    res = parseFloat(th) * parseFloat(hp);
-    tot = parseFloat(res) * 90 / 100;
-    document.getElementById("amount").innerHTML = 'Your estimated pay is:' + ' $' + tot;
-    console.log(tot);
+  if (totalHoursEmployee <= 40 && bonusEmployee > 1) {
+    res = parseFloat(bonusEmployee) + parseFloat(totalHoursEmployee) * parseFloat(hourlyPayEmployee);
+    totalAmount = parseFloat(res) * 90 / 100;
+    document.getElementById("amount").innerHTML = 'Your estimated pay is:' + ' $' + totalAmount;
+    return true;
   }
-  else if (th > 40 && bn > 0) {
-    res = (parseFloat(th) - 40) * (parseFloat(hp) * 1.5) + (40 * parseFloat(hp)) + parseFloat(bn);
-    tot = parseFloat(res) * 90 / 100;
-    document.getElementById("amount").innerHTML = 'Your estimated pay is:' + ' $' + tot;
-    console.log(tot);
+  else if (totalHoursEmployee <= 40 && bonusEmployee == 0) {
+    res = parseFloat(totalHoursEmployee) * parseFloat(hourlyPayEmployee);
+    totalAmount = parseFloat(res) * 90 / 100;
+    document.getElementById("amount").innerHTML = 'Your estimated pay is:' + ' $' + totalAmount;
+    return true;
+  }
+  else if (totalHoursEmployee > 40 && bonusEmployee > 0) {
+    res = (parseFloat(totalHoursEmployee) - 40) * (parseFloat(hourlyPayEmployee) * 1.5) + (40 * parseFloat(hourlyPayEmployee)) + parseFloat(bonusEmployee);
+    totalAmount = parseFloat(res) * 90 / 100;
+    document.getElementById("amount").innerHTML = 'Your estimated pay is:' + ' $' + totalAmount;
+    return true;
   }
   else 
-    res = (parseFloat(th) - 40) * (parseFloat(hp) * 1.5) + (40 * parseFloat(hp));
-    tot = parseFloat(res) * 90 / 100;
-    document.getElementById("amount").innerHTML = 'Your estimated pay is:' + ' $' + tot;
-     console.log(tot);
-  
+    res = (parseFloat(totalHoursEmployee) - 40) * (parseFloat(hourlyPayEmployee) * 1.5) + (40 * parseFloat(hourlyPayEmployee));
+    totalAmount = parseFloat(res) * 90 / 100;
+    document.getElementById("amount").innerHTML = 'Your estimated pay is:' + ' $' + totalAmount; 
+    return true;
+}
+
+function grossPayEmployer(hourlyPay, totalHours) {
+  let hourlyPayEmployer = document.getElementById('hourly-pay-employer').value;
+  let totalHoursEmployer = document.getElementById('total-hours-employer').value;
+  let bonusEmployer = document.getElementById('bonus-employer').value;
+  let employeesNumber = document.getElementById('employees-number').value;
+  let totalAmount = 0
+  if (hourlyPayEmployer == null || hourlyPayEmployer == "" || totalHoursEmployer == null || totalHoursEmployer == "" || employeesNumber == null || employeesNumber == "" ) {
+    return false
+  }
+  if (totalHoursEmployer <= 40 && bonusEmployer > 1) {
+    res = parseFloat(bonusEmployer) + parseFloat(totalHoursEmployer) * parseFloat(hourlyPayEmployer);
+    totalAmount = parseFloat(res) * parseFloat(employeesNumber);
+    document.getElementById("amount-employer").innerHTML = 'Your estimated pay is:' + ' $' + totalAmount;
+    return true
+  }
+  else if (totalHoursEmployer <= 40 && bonusEmployer == 0) {
+    res = parseFloat(totalHoursEmployer) * parseFloat(hourlyPayEmployer);
+    totalAmount = parseFloat(res) * parseFloat(employeesNumber);
+    document.getElementById("amount-employer").innerHTML = 'Your estimated pay is:' + ' $' + totalAmount;
+    return true
+  }
+  else if (totalHoursEmployer > 40 && bonusEmployer > 0) {
+    res = (parseFloat(totalHoursEmployer) - 40) * (parseFloat(hourlyPayEmployer) * 1.5) + (40 * parseFloat(hourlyPayEmployer)) + parseFloat(bonusEmployer);
+    totalAmount = parseFloat(res) * parseFloat(employeesNumber);
+    document.getElementById("amount-employer").innerHTML = 'Your estimated pay is:' + ' $' + totalAmount;
+    return true
+  }
+  else 
+    res = (parseFloat(totalHoursEmployer) - 40) * (parseFloat(hourlyPayEmployer) * 1.5) + (40 * parseFloat(hourlyPayEmployer));
+    totalAmount = parseFloat(res) * parseFloat(employeesNumber);
+    document.getElementById("amount-employer").innerHTML = 'Your estimated pay is:' + ' $' + totalAmount; 
+    return true
 }
 
 
 $(document).ready(function(){
- 
-  $('input[type="button"]').on('click', function(e){
-
+  
+  $('div.result input[type="button"]').on('click', function(e){
     e.preventDefault();
-    // e.preventPropagation();
-    // $('#asba').text('Your estimated pay is' + ' $ ' + tot);
+    let validationEmployee = grossPayEmployee();
+    if (validationEmployee == true) {
     $('div#result-window').removeClass('hidden').addClass('show-result-employee');
-    // $('div.input-box').addClass('hidden');
-    // $('employee-estimator').addClass('hidden');
+    } else {
+      $(' div.employee-estimator div.input-box').addClass('error');  
+    }
   });
 
   $('button.restart').on('click', function() {
-    $('div#result-window').removeClass('show-result-employee').addClass('hidden');
-    
+  $('div#result-window').removeClass('show-result-employee').addClass('hidden');  
+  $('div#show-result-employer').removeClass('show-result-employee').addClass('hidden'); 
   });
   
-
+  $('div.result-employer input[type="button"]').on('click', function(event) {
+    event.preventDefault();
+    let validationEmployer = grossPayEmployer();
+    if (validationEmployer == true) {
+    $('div#show-result-employer').removeClass('hidden').addClass('show-result-employee'); 
+    } else { 
+      $('div.employer-estimator div.input-box').addClass('error');
+    }
+  });
+ 
 });
 
